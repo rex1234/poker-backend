59e5ec5255c19a2da072f05dd99343827f637496
Jan Mjartan <mjartan.jan@gmail.com>
2020-04-14 15:37:40 +0200

    https

--- src/main/java/io/pokr/network/WebEngine.kt
+++ src/main/java/io/pokr/network/WebEngine.kt
@@ -2,7 +2,10 @@ package io.pokr.network
 
 
 import io.github.cdimascio.dotenv.dotenv
+import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.HttpsRedirect
 import io.ktor.http.content.file
 import io.ktor.http.content.files
 import io.ktor.http.content.static
@@ -10,30 +13,74 @@ import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.route
 import io.ktor.routing.routing
+import io.ktor.server.engine.applicationEngineEnvironment
+import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
+import io.ktor.server.engine.sslConnector
 import io.ktor.server.netty.Netty
+import java.io.File
+import java.io.FileInputStream
+import java.security.KeyStore
+import java.security.KeyStore.*
 import kotlin.concurrent.thread
 
 class WebEngine {
     fun start() {
         thread {
-            embeddedServer(Netty, dotenv()["WEB_PORT"]!!.toInt()) {
-                routing {
-                    route("api") {
-                        get("/game_state") {
-                        }
+            embeddedServer(Netty, applicationEngineEnvironment {
+                module {
+                    main()
+                }
 
-                        get("/version") {
-                            call.respondText("v1.0")
+                connector {
+                    port = dotenv()["WEB_PORT"]!!.toInt()
+                }
+
+                val keyStoreFile = File(dotenv()["KEYSTORE_PATH"])
+
+                if(keyStoreFile.exists()) {
+                    val keystorePw = dotenv()["KEYSTORE_PASSWORD"]!!.toCharArray()
+                    val keyStoreAlias = dotenv()["KEYSTORE_ALIAS"]!!
+
+                    val keyStore = getInstance("JKS").apply {
+                        FileInputStream(keyStoreFile).use {
+                            load(it, keystorePw)
                         }
                     }
 
-                    static {
-                        files("web")
-                        file("/", "web/game.html")
+                    sslConnector(
+                        keyStore = keyStore,
+                        keyAlias = keyStoreAlias,
+                        keyStorePassword = { keystorePw },
+                        privateKeyPassword = { keystorePw }) {
+                        port = 443
+                        keyStorePath = keyStoreFile
                     }
                 }
-            }.start(wait = true)
+            }).start(wait = true)
         }
+    }
+
+    fun Application.main() {
+        if(File(dotenv()["KEYSTORE_PATH"]).exists()) {
+            install(HttpsRedirect)
+        }
+
+        routing {
+            route("api") {
+                get("/game_state") {
+                }
+
+                get("/version") {
+                    call.respondText("v1.0")
+                }
+            }
+
+            static {
+                files("web")
+                file("/", "web/game.html")
+            }
+        }
+
     }
 }
