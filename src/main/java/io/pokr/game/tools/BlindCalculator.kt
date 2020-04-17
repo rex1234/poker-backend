52d8528dc92b9efcf206c521917b581706eb9584
Jan Mjartan <mjartan.jan@gmail.com>
2020-04-17 18:49:01 +0200

    object refactor

--- src/main/java/io/pokr/game/tools/BlindCalculator.kt
+++ src/main/java/io/pokr/game/tools/BlindCalculator.kt
@@ -1,12 +1,11 @@
 package io.pokr.game.tools
 
-class BlindCalculator {
+object BlindCalculator {
 
-    companion object {
-        val smallBlinds = listOf(10, 20, 30, 50, 75, 100, 150, 250, 400, 600, 800, 1000)
+    val smallBlinds = listOf(10, 20, 30, 50, 75, 100, 150, 250, 400, 600, 800, 1000)
 
-        // TODO: Implement blind calculation
-        fun nextBlind(currentBlind: Int) =
-            smallBlinds.firstOrNull { it > currentBlind } ?: currentBlind * 2
-    }
+    // TODO: Implement blind calculation
+    fun nextBlind(currentBlind: Int) =
+        smallBlinds.firstOrNull { it > currentBlind } ?: currentBlind * 2
+
 }
--- src/main/java/io/pokr/game/tools/WinningsCalculator.kt
+++ src/main/java/io/pokr/game/tools/WinningsCalculator.kt
@@ -1,62 +1,59 @@
 package io.pokr.game.tools
 
-class WinningsCalculator {
+object WinningsCalculator {
 
-    companion object {
+    // TODO: add remaining chip to SB / BB (in case of 1 chip split)
+    fun calculateWinnings(results_: List<HandComparator.PlayerHandComparisonResult>) {
+        results_.map { it.player }.forEach {
+            it.chips -= it.currentBet
+        }
 
-        // TODO: add remaining chip to SB / BB (in case of 1 chip split)
-        fun calculateWinnings(results_: List<HandComparator.PlayerHandComparisonResult>) {
-            results_.map { it.player }.forEach {
-                it.chips -= it.currentBet
-            }
+        val results = results_.sortedBy { it.player.currentBet }
+        var pot = results.sumBy { it.player.currentBet }
 
-            val results = results_.sortedBy { it.player.currentBet }
-            var pot = results.sumBy { it.player.currentBet }
+        var reminder = 0
+        var taken = 0
 
-            var reminder = 0
-            var taken = 0
+        for(i in 0 until results.size) {
+            var splitting = 1
 
-            for(i in 0 until results.size) {
-                var splitting = 1
+            for (k in i + 1 until results.size) {
 
-                for (k in i + 1 until results.size) {
+                // lost
+                if(results[i].rank > results[k].rank) {
+                    reminder += results[i].player.currentBet
+                    results[i].player.currentBet = 0
+                    break
+                }
 
-                    // lost
-                    if(results[i].rank > results[k].rank) {
-                        reminder += results[i].player.currentBet
-                        results[i].player.currentBet = 0
-                        break
+                // split on the way
+                if(results[i].rank == results[k].rank) {
+                    splitting++
+                }
+
+                // not lost
+                if(k == results.size - 1) {
+                    var winnings = (results[i].player.currentBet - taken) * (results.size - i) + reminder
+                    taken = results[i].player.currentBet
+                    winnings /= splitting
+                    results[i].player.currentBet = winnings
+                    reminder = 0
+                    if(splitting > 1) {
+                        reminder = winnings * (splitting - 1);
                     }
+                    splitting = 0
 
-                    // split on the way
-                    if(results[i].rank == results[k].rank) {
-                        splitting++
-                    }
-
-                    // not lost
-                    if(k == results.size - 1) {
-                        var winnings = (results[i].player.currentBet - taken) * (results.size - i) + reminder
-                        taken = results[i].player.currentBet
-                        winnings /= splitting
-                        results[i].player.currentBet = winnings
-                        reminder = 0
-                        if(splitting > 1) {
-                            reminder = winnings * (splitting - 1);
-                        }
-                        splitting = 0
-
-                        pot -= winnings
-                    }
+                    pot -= winnings
                 }
             }
+        }
 
-            results.last().player.currentBet = pot
+        results.last().player.currentBet = pot
 
-            results.forEach {
-                it.player.lastWin = it.player.currentBet
-                it.player.chips += it.player.currentBet
-                it.player.currentBet = 0
-            }
+        results.forEach {
+            it.player.lastWin = it.player.currentBet
+            it.player.chips += it.player.currentBet
+            it.player.currentBet = 0
         }
     }
 }
