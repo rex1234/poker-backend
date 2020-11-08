e969deceb7a58d62b11bd40415ae4c114ad43d01
Radek Zábranský <radek@salsitasoft.com>
2020-11-08 10:59:30 +0100

    Add update.sh to Git and improve it

---
+++ update.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+set -e
+
+log_progress() {
+  echo "============================================================"
+  echo "$1"
+  echo "============================================================"
+  echo
+}
+
+DIRECTORY=$(pwd)
+
+case $DIRECTORY in
+  "/root/projects/poker-backend")
+    SERVICE="pokrio"
+    ;;
+  "/root/projects/poker-backend-dev")
+    SERVICE="pokriodev"
+    ;;
+  *)
+    echo "Not in a deployment directory, doing nothing"
+    exit 1
+    ;;
+esac
+
+echo "Updating ${SERVICE} in ${DIRECTORY}..."
+
+log_progress "Fetching new sources"
+git fetch --all
+git reset --hard origin/master
+
+log_progress "Building the backend"
+./gradlew fatJar
+cp ./build/libs/pokrio-1.0.jar pokrio.jar
+
+log_progress "Minifying JavaScript"
+echo `uglifyjs web/js/game.js` > web/js/game.js
+echo `uglifyjs web/js/animations.js` > web/js/animations.js
+echo `uglifyjs web/js/sockets.js` > web/js/sockets.js
+
+log_progress "Restarting ${SERVICE} service"
+systemctl restart ${SERVICE}.service
+
+echo "All done!"
