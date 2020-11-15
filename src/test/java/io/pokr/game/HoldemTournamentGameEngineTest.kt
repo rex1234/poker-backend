4890143934b7b9bd641d22f9225adfc687abad2c
Radek Zábranský <radek@salsitasoft.com>
2020-11-15 14:17:30 +0100

    Make tests fail

--- src/test/java/io/pokr/game/HoldemTournamentGameEngineTest.kt
+++ src/test/java/io/pokr/game/HoldemTournamentGameEngineTest.kt
@@ -314,7 +314,7 @@ class HoldemTournamentGameEngineTest {
         engine.nextPlayerMove(bigBlind.uuid, PlayerAction(PlayerAction.Action.CALL))
 
         Assert.assertEquals(true, button.isDealer)
-        Assert.assertEquals(true, button.isOnMove)
+        Assert.assertEquals(false, button.isOnMove)
     }
 
 }
