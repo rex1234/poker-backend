200957dc048c91a4c5788ffc40b631d35a1ab561
Radek Zábranský <radek@salsitasoft.com>
2020-11-19 07:45:35 +0100

    Make additionalInfo optional and init to null

--- src/main/java/io/pokr/game/exceptions/GameException.kt
+++ src/main/java/io/pokr/game/exceptions/GameException.kt
@@ -3,5 +3,5 @@ package io.pokr.game.exceptions
 class GameException(
     val code: Int,
     message: String,
-    val additionalInfo: String = "",
+    val additionalInfo: String? = null,
 ) : Exception(message)
