d38ef9b011801111e83cd79c99a59cc1212afc15
zbrcz <radek@salsitasoft.com>
2020-12-15 16:37:22 +0100

    Fix game breaking when not enough chips for blind, return extra chips, etc. (#27)

--- src/main/java/io/pokr/game/tools/WinningsCalculator.kt
+++ src/main/java/io/pokr/game/tools/WinningsCalculator.kt
@@ -11,35 +11,35 @@ object WinningsCalculator {
         val results = results_.sortedBy { it.player.currentBet }
         var pot = results.sumBy { it.player.currentBet }
 
-        var reminder = 0
+        var remainder = 0
         var taken = 0
 
-        for(i in 0 until results.size) {
+        for (i in 0 until results.size) {
             var splitting = 1
 
             for (k in i + 1 until results.size) {
 
                 // lost
-                if(results[i].rank > results[k].rank) {
-                    reminder += results[i].player.currentBet
-                    results[i].player.currentBet = 0
+                if (results[i].rank > results[k].rank) {
+                    remainder += results[i].player.currentBet
+                    results[i].winnings = 0
                     break
                 }
 
                 // split on the way
-                if(results[i].rank == results[k].rank) {
+                if (results[i].rank == results[k].rank) {
                     splitting++
                 }
 
                 // not lost
-                if(k == results.size - 1) {
-                    var winnings = (results[i].player.currentBet - taken) * (results.size - i) + reminder
+                if (k == results.size - 1) {
+                    var winnings = (results[i].player.currentBet - taken) * (results.size - i) + remainder
                     taken = results[i].player.currentBet
                     winnings /= splitting
-                    results[i].player.currentBet = winnings
-                    reminder = 0
-                    if(splitting > 1) {
-                        reminder = winnings * (splitting - 1);
+                    results[i].winnings = winnings
+                    remainder = 0
+                    if (splitting > 1) {
+                        remainder = winnings * (splitting - 1);
                     }
                     splitting = 0
 
@@ -48,12 +48,11 @@ object WinningsCalculator {
             }
         }
 
-        results.last().player.currentBet = pot
+        results.last().winnings = pot
 
         results.forEach {
-            it.player.lastWin = it.player.currentBet
-            it.player.chips += it.player.currentBet
-            it.player.currentBet = 0
+            it.player.lastWin = it.winnings
+            it.player.chips += it.winnings
         }
     }
 }
